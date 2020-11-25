ecc0dc73106afd11afb717518ede161361344d4e
YourName <815721035@qq.com>
2020-11-25 23:08:16 +0800

    集成druid连接池 修复菜单编辑bug

---
+++ src/main/java/com/democxy/modules/sys/controller/monitor/DruidController.java
@@ -0,0 +1,23 @@
+package com.democxy.modules.sys.controller.monitor;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * druid 监控
+ *
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/monitor/data")
+public class DruidController {
+    private String prefix = "/druid";
+
+    @GetMapping()
+    public String index()
+    {
+        return"redirect:"+prefix + "/index";
+    }
+}
