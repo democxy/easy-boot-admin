51d3f673a57925be5947c42b8b6e89248fff9515
YourName <815721035@qq.com>
2021-03-22 21:45:16 +0800

    删除模板内容管理

--- src/main/java/com/democxy/modules/gen/controller/GenCodeController.java
+++ src/main/java/com/democxy/modules/gen/controller/GenCodeController.java
@@ -1,18 +1,10 @@
 package com.democxy.modules.gen.controller;
 
 import com.democxy.common.annotation.PassLogin;
-import com.democxy.common.annotation.Permission;
 import com.democxy.common.config.ProjectConfig;
-import com.democxy.common.utils.StringUtils;
-import com.democxy.modules.gen.entity.GenTable;
-import com.democxy.modules.gen.entity.GenTableColumn;
-import com.democxy.modules.gen.entity.field.GenTableColumnField;
 import com.democxy.modules.gen.service.GenTableColumnService;
 import com.democxy.modules.gen.service.GenTableService;
-import com.democxy.modules.gen.service.TemplateInfoService;
 import com.democxy.modules.gen.utils.GenCodeUtil;
-import com.democxy.modules.gen.vo.PreViewCode;
-import com.google.common.collect.Lists;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -22,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
 
@@ -46,8 +37,6 @@ public class GenCodeController {
     @Autowired
     FreeMarkerConfig freeMarkerConfig;
     @Autowired
-    TemplateInfoService templateInfoService;
-    @Autowired
     ProjectConfig projectConfig;
     @Autowired
     GenCodeUtil genCodeUtil;
@@ -58,32 +47,6 @@ public class GenCodeController {
 
     @GetMapping("test")
     public String gen(){
-        /**/
-        String temStr = templateInfoService.getById("830C7ABB2647410188C6682BE273E2C9").getContent();
-        try {
-            StringWriter result = new StringWriter();
-            Template template = new Template("template", new StringReader(temStr),
-                    new Configuration(Configuration.VERSION_2_3_23));
-            Map<String,Object> map = new HashMap<>();
-            map.put("packageName","com.democxy.test");
-            map.put("moduleName","gen");
-            map.put("ClassName","TemplateInfo");
-            String string = FreeMarkerTemplateUtils.processTemplateIntoString(template, map);
-//            FileUtils.writeStringToFile(new File("F://gencode/test.java"),string,"UTF-8");
-            System.out.println("st0:   " + string);
-            Configuration configuration = freeMarkerConfig.getConfiguration();
-            configuration.setDirectoryForTemplateLoading(new File(projectConfig.getBasepath()));
-            Template template1 = configuration.getTemplate("test.ftl", "utf-8");
-            String string1 = FreeMarkerTemplateUtils.processTemplateIntoString(template1, map);
-            System.out.println("st1:  " + string1);
-            FileUtils.writeStringToFile(new File("F://gencode/test2.java"),string1,"UTF-8");
-
-
-
-            return string;
-        } catch (IOException | TemplateException e) {
-            e.printStackTrace();
-        }
         return "error";
     }
 
--- src/main/java/com/democxy/modules/gen/service/TemplateInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.democxy.modules.gen.service;
-
-import com.democxy.common.global.BaseService;
-import com.democxy.modules.gen.entity.TemplateInfo;
-import com.democxy.modules.gen.entity.field.TemplateInfoField;
-
-public interface TemplateInfoService extends BaseService<TemplateInfo, TemplateInfoField> {
-
-}
